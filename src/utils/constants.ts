b0edc0df2a2676d2e5f2c703c075be70f9592fcc
Dalembert Monzón Urrutia <dalembert.dmu@gmail.com>
2025-04-27 00:09:36 +0900

    feat: Add base header, hero and sections

---
+++ src/utils/constants.ts
@@ -0,0 +1,22 @@
+export const sections = {
+  experience: {
+    title: "Experiencia",
+    label: "experiencia",
+    url: "/#experience",
+  },
+  projects: {
+    title: "Proyectos",
+    label: "proyectos",
+    url: "/#projects",
+  },
+  aboutMe: {
+    title: "Sobre mí",
+    label: "sobre-mí",
+    url: "/#about-me",
+  },
+  contact: {
+    title: "Contacto",
+    label: "contacto",
+    url: "mailto:dalembert.dmu@gmail.com",
+  },
+};
